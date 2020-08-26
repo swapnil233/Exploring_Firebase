1f665e619f1b70360c601f18a792c0e4903c9cfd
Hasan Iqbal <36313876+swapnil233@users.noreply.github.com>
2020-08-25 22:00:06 -0400

    Initial Commit
    Created the boilerplates. Added Firebase config object to firebase.js. Included the auth, firestore, storage and database SDKs in the index.html as required.

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDjDvDJ1hKyCLx0P7rDGXdbOJ20I0ruW0o",
+    authDomain: "fir-databases-45e9c.firebaseapp.com",
+    databaseURL: "https://fir-databases-45e9c.firebaseio.com",
+    projectId: "fir-databases-45e9c",
+    storageBucket: "fir-databases-45e9c.appspot.com",
+    messagingSenderId: "194865326627",
+    appId: "1:194865326627:web:603eb0b086215aac2cdf96"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase.database())
